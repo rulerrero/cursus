79ddd2fe55282fba6c5faafe59193fff71652937
rulerrero <104133644+rulerrero@users.noreply.github.com>
2022-04-25 13:06:05 +0200

    work_in_house

---
+++ cursus/libft/libft.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rherrero <rherrero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 13:50:07 by rherrero          #+#    #+#             */
+/*   Updated: 2022/04/25 12:15:54 by rherrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *s, int c, size_t n);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+int		ft_strlen(char *s);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
